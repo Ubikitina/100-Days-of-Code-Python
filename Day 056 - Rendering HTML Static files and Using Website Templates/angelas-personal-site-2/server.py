a61a9400e28b2490c13702cb940cb252c1bac4ee
Maialen <34455658+Ubikitina@users.noreply.github.com>
2023-11-15 22:06:37 +0100

    Day 56 completed.

---
+++ Day 056 - Rendering HTML Static files and Using Website Templates/angelas-personal-site-2/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # Create an instance of this class
+
+
+@app.route('/')  # Use the route() decorator to tell Flask what URL should trigger our function. We are using '/', so we will be showing the home page
+def home():  # This function returns the message we want to display in the user’s browser
+    return render_template('index.html')
+
+
+
+# To run it by using our standard run and stop buttons in PyCharm, we will use the below code
+if __name__ == "__main__":
+    app.run(debug=True)  # This will allow us to run our app in debug mode
