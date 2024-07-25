42da263cb7d6ef1200521c56bd5074f0ae78b741
Maialen <34455658+Ubikitina@users.noreply.github.com>
2024-07-25 20:43:57 +0200

    Initial commit for Day 89

---
+++ Day 089 - Portfolio Project - Todo List/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, URL
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Task Title", validators=[DataRequired()])
+    body = TextAreaField("Task Description", validators=[DataRequired()])
+    submit = SubmitField("Submit Task")
+
+
+# Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+# Create a LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
+
+# Create a CommentForm so users can leave comments below posts
+class CommentForm(FlaskForm):
+    comment_text = TextAreaField("Add your comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
+
