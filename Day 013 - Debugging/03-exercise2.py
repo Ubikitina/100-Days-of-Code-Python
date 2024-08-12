aec5511d066dc163ad40a2e1a3c4864312230605
Maialen <34455658+Ubikitina@users.noreply.github.com>
2024-08-12 18:39:43 +0200

    Remove empty file

--- Day 013 - Debugging/03-exercise2.py
+++ Day 013 - Debugging/03-exercise2.py
@@ -1,27 +1,21 @@
 # BUGGY CODE
-# year = input("Which year do you want to check?")
-
-# if year % 4 == 0:
-#   if year % 100 == 0:
-#     if year % 400 == 0:
-#       print("Leap year.")
-#     else:
-#       print("Not leap year.")
+# for number in range(1, 101):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
 #   else:
-#     print("Leap year.")
-# else:
-#   print("Not leap year.")
-  
-# CORRECTED CODE
-year = int(input("Which year do you want to check?"))
+#     print([number])
 
-if year % 4 == 0:
-  if not year % 100 == 0:
-    if not year % 400 == 5:
-      print("Leap year.")
-    else:
-      print("Not leap year.")
+# CORRECTED CODE
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
   else:
-    print("Leap year.")
-else:
-  print("Not leap year.")
+    print(number)
